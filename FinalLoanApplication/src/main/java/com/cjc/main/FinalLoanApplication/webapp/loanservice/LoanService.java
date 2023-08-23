4d3d342062cd1b8db170ce83213e5298e704e782
shreya-naidu <shreyanaidu2001@gmail.com>
2023-08-23 11:59:14 +0530

    bad commit

--- FinalLoanApplication/src/main/java/com/cjc/main/FinalLoanApplication/webapp/loanservice/LoanService.java
+++ FinalLoanApplication/src/main/java/com/cjc/main/FinalLoanApplication/webapp/loanservice/LoanService.java
@@ -3,7 +3,6 @@ package com.cjc.main.FinalLoanApplication.webapp.loanservice;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cjc.main.FinalLoanApplication.webapp.entity.EnquiryDetails;
-import com.cjc.main.FinalLoanApplication.webapp.entity.MailDetails;
 import com.cjc.main.FinalLoanApplication.webapp.entity.Users;
 
 public interface LoanService {
